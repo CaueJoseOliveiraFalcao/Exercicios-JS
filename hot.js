72d161a5b56ccad0439658adaa7b6655ee7cec22
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2022-08-23 12:52:21 -0300

    wqeqwe
    qwe

--- hot.js
+++ hot.js
@@ -1,22 +1,41 @@
+'use strict'
 class Car{
-    constructor(speed){
-        this.speed = speed
+    constructor(model){
+        this.model = model
     }
     presente(){
         return console.log('receba porra')
     }
 }
-class Model extends Car{
-    constructor(speed,mod){
-        super(speed)
-        this.mod = mod
-    }
-    mostrar(){
-        return this.presente()
-    }
-}
 
-const meucarro = new Car('100')
-console.log(meucarro.speed)
-const meubyu = new Model('100' , 'fort fort')
-meubyu.mostrar()
+
+const meucarro = new Car('Ford')
+
+let hello = (val) => console.log('hello' + ' ' + val)
+
+hello(3)
+
+const numero = 8
+
+
+
+console.log(numero)
+
+function calculate(a, b) {
+    const add = a + b;
+    const subtract = a - b;
+    const multiply = a * b;
+    const divide = a / b;
+  return [add , subtract , multiply , divide]
+    
+  }
+
+  const [add, subtract, multiply, divide] = calculate(4, 7);
+  console.log(add)
+  
+
+  const numbers = [1, 2, 3, 4, 5, 6];
+
+  let [as , asd , ...rest] = numbers
+
+  console.log(as , asd , rest)
